284062ab14c588b409084d0cb9bd75784dbec6b8
Jakob Laurell <jakob.laurell@polestar.com>
2022-01-10 16:42:22 +0100

    Solve day17

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, hash::Hash, alloc::System, ops::RangeBounds};
+use std::{collections::{HashMap, HashSet}};
 
 
 fn roll(mut pt: (i64, i64, i64)) -> (i64, i64, i64) {
@@ -28,14 +28,14 @@ fn rotations(mut pt: (i64, i64, i64)) -> Vec<(i64, i64, i64)> {
 }
 
 fn main() {
-    let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
     let mut lines = input.trim().split("\r\n");
     let mut scanners = Vec::new();
 
     loop {
         let mut beacons = Vec::new();
         for _ in 0..24 {
-            beacons.push(Vec::new());
+            beacons.push(HashSet::new());
         }
         if lines.next().is_none() {
             break;
@@ -49,68 +49,113 @@ fn main() {
             let y = xyz.next().unwrap().parse::<i64>().unwrap();
             let z = xyz.next().unwrap().parse::<i64>().unwrap();
             for (i, pt) in rotations((x, y, z)).into_iter().enumerate() {
-                beacons[i].push(pt);
+                beacons[i].insert(pt);
             }
         }
         scanners.push(beacons);
     }
     
+    let mut scanner_offsets = Vec::new();
+    let mut map = HashSet::new();
+    let pos = &scanners[0];
+    for p in pos[0].iter() {
+        map.insert(*p);
+    }
+    println!("{}", map.len());
+    scanners.remove(0);
+    check_scanners(&mut map, &mut scanners, &mut scanner_offsets);
 
-    let mut systems: Vec<HashSet<usize>> = Vec::new();
-    
-    loop {
-        let mut aligned= Vec::new();
-        let mut next_realignment = None;
-        'outer: for (i, scanner) in scanners.iter().enumerate() {
-            for (j, other_scanner) in scanners.iter().enumerate().skip(i+1) {
-                let other_beacons = &other_scanner[0];
+    while scanners.len() != 0 {
+        let mut sub_map = scanners.pop().unwrap().pop().unwrap();
+        check_scanners(&mut sub_map, &mut scanners, &mut scanner_offsets);
 
-                for (rotation_idx, beacons) in scanner.iter().enumerate() {
-                    let mut potential_offsets: HashMap<(i64, i64, i64), usize> = HashMap::new();
-                    for beacon in beacons {
-                        for other_beacon in other_beacons {
-                            let offset = (beacon.0 -other_beacon.0, beacon.1 -other_beacon.1, beacon.2 -other_beacon.2);
-                        
+        let mut beacons = Vec::new();
+        for _ in 0..24 {
+            beacons.push(HashSet::new());
+        }
+
+        for (x, y, z) in sub_map.iter() {
+            for (i, pt) in rotations((*x, *y, *z)).into_iter().enumerate() {
+                beacons[i].insert(pt);
+            }
+        }
+        scanners.push(beacons);
+
+        check_scanners(&mut map, &mut scanners, &mut scanner_offsets);
+    }
+    println!("Total numbers of beacons: {}", map.len()); // 372
+
+    let mut longest = 0;
+    // part B, largest distance between 
+    for key in scanner_offsets.iter() {
+        for other_key in scanner_offsets.iter() {
+            let dist = distance(*key, *other_key);
+            if dist > longest {
+                longest = dist;
+            }
+        }
+    }
+    println!("Longest Manhattan distance: {}", longest); // 12241
+}
+
+fn distance(beacon_a: (i64, i64, i64), beacon_b: (i64, i64, i64)) -> i64{
+    (beacon_a.0 - beacon_b.0).abs() + (beacon_a.1 - beacon_b.1).abs() + (beacon_a.2 - beacon_b.2).abs() 
+}
+
+fn check_scanners(map: &mut HashSet<(i64, i64, i64)>, scanners: &mut Vec<Vec<HashSet<(i64, i64, i64)>>>, scanner_offsets: &mut Vec<(i64, i64 ,i64)>) {
+    loop{
+        let mut prev_length = scanners.len();
+        let mut match_found = false;
+        let mut index = 0;
+        'inner: for (idx, scanner_sets) in scanners.iter_mut().enumerate() {
+            
+            for other_beacons in scanner_sets.iter() {
+                let mut potential_offsets: HashMap<(i64, i64, i64), usize> = HashMap::new();
+
+                for beacon in map.iter() {
+                    for other_beacon in other_beacons {
+                        let offset = (
+                            beacon.0 - other_beacon.0,
+                            beacon.1 - other_beacon.1,
+                            beacon.2 - other_beacon.2,
+                        );
                         *potential_offsets.entry(offset).or_default() += 1;
-                        }
                     }
-                    let mut potential_offset: Vec<_> = potential_offsets
-                        .into_iter()
-                        .filter(|(_, count)| *count >= 12)
-                        .map(|(offset, _)| offset)
-                        .collect();
+                }
+                
+                let mut potential_offsets: Vec<_> = potential_offsets
+                            .into_iter()
+                            .filter(|entry| entry.1 >= 12)
+                            .map(|(offset, _)| offset)
+                            .collect();
 
-                    if let Some(offset) = potential_offset.pop() {
-                        aligned.push((i, j, rotation_idx, offset));
-                        if rotation_idx != 0 {
-                            next_realignment = Some((i, j, rotation_idx));
-                            break 'outer;
+                assert!(potential_offsets.is_empty() || potential_offsets.len() == 1);
+                if let Some(offset) = potential_offsets.pop() {
+                    println!("Matching set found!");
+                    scanner_offsets.push(offset);
+                    let mut count = 0;
+                    for other_beacon in other_beacons.iter() {
+                        let new_beacon = (other_beacon.0 + offset.0, other_beacon.1 + offset.1, other_beacon.2 + offset.2);
+                        
+                        if map.contains(&new_beacon){
+                            count += 1;
+                        } else {
+                            map.insert(new_beacon);
                         }
                     }
-                    eprintln!("potential offset: {:?}", potential_offset)
+                    println!("{} matching beacons", count);
+                    match_found = true;
+                    index = idx;
+                    break 'inner
                 }
             }
         }
-        if let Some((i, j, rot_idx)) = next_realignment {
-            eprintln!("next");
-            let system = systems.iter()
-                .filter(|items| items.contains(i) || items.contains(j));
-        } else {
-            eprint!("{:#?}", aligned);
+        if match_found {
+            scanners.remove(index);
+            println!("Scanners left: {}", scanners.len());
+        }
+        if scanners.len() == prev_length {
             break;
         }
-    }
-
-    //eprintln!("{}", systems.len());
-}
-
-
-fn append_scanner(map: &mut HashMap<(i64, i64, i64), i64>, scanner: &Vec<(i64, i64, i64)>) {
-    for pos in scanner.iter() {
-        // avoid duplicate inputs
-        if map.contains_key(pos) {
-            continue;
-        } 
-        map.insert(*pos, 1);
     }
 }

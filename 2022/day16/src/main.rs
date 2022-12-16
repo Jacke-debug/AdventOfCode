e6db72f96ee8d56462109df768e7adf30e04c4de
Jakob Laurell <jakob.laurell@polestar.com>
2022-12-16 17:52:10 +0100

    Start day 16

--- 2022/day16/src/main.rs
+++ 2022/day16/src/main.rs
@@ -2,18 +2,27 @@ use std::{collections::HashMap, borrow::Borrow, hash::Hash};
 
 #[derive(Debug)]
 #[derive(Clone)]
+#[derive(Copy)]
+struct Connection<'a> {
+    dest: &'a str,
+    cost: i32,
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
 struct Node<'a> {
     name: &'a str,
     rate: i32,
     state: bool,
-    connections: Vec<&'a str>,
+    connections: Vec<Connection<'a>>,
 }
 
 fn part_a(input: &str) -> i32 {
     let mut data = input.trim().split("\r\n");
     let mut score = 0;
 
-    let mut map: HashMap<&str, Node> = HashMap::new();
+    let mut map: Vec<Node> = Vec::new();
+    let mut zero_valves: HashMap<&str, Node> = HashMap::new();
 
     while let Some(line) = data.next() {
         let (name, rest) = line.split_once(" has").unwrap();
@@ -22,80 +31,146 @@ fn part_a(input: &str) -> i32 {
         let (_, rate) = rate.split_once("=").unwrap();
         let rate: i32 = rate.parse().unwrap();
         match rest.split_once("valves ") {
-            Some((_, connections)) => {
-                let cons = connections.split(", ");
-                let mut connections: Vec<&str> = cons.into_iter().collect();
-                if rate != 0 {
-                    connections.push(name);
+            Some((_, con_str)) => {
+                let mut connections = Vec::new();
+                for connection in con_str.split(", ").into_iter() {
+                    connections.push(Connection{
+                        dest: connection, 
+                        cost: 1
+                    })
                 }
-                
-                let node = Node{
+                let mut node = Node{
                     name,
                     rate,
                     state: false,
                     connections: connections,
                 };
-                map.insert(name, node);
+                if rate == 0 && name != "AA" {
+                    zero_valves.insert(name, node);
+                } else {
+                    if name == "AA" {
+                        node.state = true;
+                    }
+                    map.push(node);
+                }
             },
             None => {
                 let (_, connection) = rest.split_once("valve ").unwrap();
-                let mut connections: Vec<&str> = vec!(connection);
-                if rate != 0 {
-                    connections.push(name);
-                }
-                let node = Node{
+                let mut connections = Vec::new();
+                connections.push(Connection{
+                    dest: connection, 
+                    cost: 1
+                });
+                let mut node = Node{
                     name,
                     rate,
                     state: false,
                     connections: connections,
                 };
-                map.insert(name, node);
+                if rate == 0 && name != "AA"{
+                    zero_valves.insert(name, node);
+                } else {
+                    if name == "AA" {
+                        node.state = true;
+                    }
+                    map.push(node);
+                }
             },
         };
     }
     println!("{:?}", map);
 
-    let mut position = "AA";
-    let mut time = 0;
-    let mut total_flow = 0;
-    let mut flow_rate = 0;
-    let mut max_flow: Vec<i32> = Vec::new();
-    make_move(&map, position, total_flow, flow_rate, time, &mut max_flow);
+    // optimize map
+
+    for valve in map.iter_mut() {
+        for _ in 0..30 {
+            let mut new_tunnels = Vec::new();
+            let mut existing_targets = Vec::new();
+            for aa in &valve.connections {
+                existing_targets.push(aa.dest);
+            }
+            for tunnel in &valve.connections {
+                // if tunnel leads to a zero valve
+                if let Some(zero_valve) = zero_valves.get(tunnel.dest) {
+                    for zer_tunnel in &zero_valve.connections {
+                        if !existing_targets.contains(&zer_tunnel.dest) {
+                            new_tunnels.push(Connection{
+                                dest: zer_tunnel.dest,
+                                cost: zer_tunnel.cost + tunnel.cost,
+                            })
+                        }
+                    }
+                } else {
+                    new_tunnels.push(*tunnel)
+                }
+            }
+            println!("\n {:?} \n\n", valve.connections);
+            valve.connections = new_tunnels;
+            println!("{:?}", valve.connections);
+        }
+    }
+
+    let mut comp_map = HashMap::new();
+    for valve in map.iter_mut() {
+        let mut new_tunnels = Vec::new();
+        for tunnel in &valve.connections {
+            if let Some(zero_valve) = zero_valves.get(tunnel.dest) {
+            } else {
+                if tunnel.dest == valve.name {
+                    // dont add conections to self
+                } else {
+                    new_tunnels.push(*tunnel)
+                }
+            }
+        }
+        valve.connections = new_tunnels;
+        comp_map.insert(valve.name, valve.clone());
+    }
+
+    println!();
+    println!();
+    for (_, item) in comp_map.iter() {
+        println!("{:?}", item);
+    }
+    
+    let time = 0;
+    let flow_rate = 0;
+    let total_flow = 0;
+    let position = "AA";
+    let mut max_flow = Vec::new();
+    make_move(comp_map, position, total_flow, flow_rate, time, &mut max_flow);
 
     score = *max_flow.iter().max().unwrap();
     return score
 }
 
-fn make_move(map: &HashMap<&str, Node>, position: &str, total_flow: i32, flow_rate: i32, time: i32, max_flow: &mut Vec<i32>) {
-    let node = map.get_mut(position).unwrap();
+fn make_move(map: HashMap<&str, Node>, position: &str, total_flow: i32, flow_rate: i32, time: i32, max_flow: &mut Vec<i32>) {
+    let node = map.get(position).unwrap();
     let mut possible_moves = node.connections.clone();
-    if node.rate == 0 || !node.state {
-        possible_moves.pop(); // remove conneciton to self
+
+    if !node.state {
+        possible_moves.push(Connection{dest: position, cost: 1}); // add posibility to open vault   
     }
+
     for this_move in possible_moves {
-        let new_total_flow = total_flow + flow_rate;
+        let new_total_flow = total_flow + flow_rate*this_move.cost;
         
         let mut new_flow_rate = flow_rate;
-        let new_position = this_move;
-        if this_move == position {
+        let mut new_map = map.clone();
+        if this_move.dest == position {
             new_flow_rate += node.rate;
-            node.state = true;
+            new_map.get_mut(position).unwrap().state = true;
+            // remove visitied states?
         }
-        let new_time = time + 1;
-        if new_time == 30 {
+
+        let new_time = time + this_move.cost;
+        if new_time > 20 {
             max_flow.push(new_total_flow);
             return;
-        } else if new_time > 15 && new_total_flow < 40 {
-            return;
-        } else if new_time > 20 && new_total_flow < 60 {
-            return;
-        } else if new_time > 25 && new_total_flow < 60 {
-            return;
         } else {
-            make_move(map, new_position, new_total_flow, new_flow_rate, new_time, max_flow);
+            make_move(new_map, this_move.dest, new_total_flow, new_flow_rate, new_time, max_flow);
         }
     }
-    
 }
 
 

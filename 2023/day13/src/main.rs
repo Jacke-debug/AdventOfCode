59427f0e957193cdb3d47c2eb9293d993e87a249
Jakob Laurell <jakob.laurell@polestar.com>
2023-12-13 11:15:41 +0100

    day 13 part a

---
+++ 2023/day13/src/main.rs
@@ -0,0 +1,75 @@
+use std::{collections::HashMap, io::empty};
+
+fn print_map(map: &Vec<Vec<char>>) {
+    println!();
+    for row in map {
+        for &cell in row {
+            print!("{}", cell);
+        }
+        println!();
+    }
+}
+
+fn transpose(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let num_rows = map.len();
+    let num_columns = map[0].len();
+    (0..num_columns)
+        .map(|col_index| {
+            (0..num_rows)
+                .map(|row_index| map[row_index][col_index])
+                .collect()
+        })
+        .collect()
+}
+
+fn find_mirroring_line(map: &Vec<Vec<char>>) -> Option<usize> {
+    let height = map.len();
+    'outer: for idx in 1..height {
+        let mut pre_mirror: Vec<Vec<char>> = map[0..idx].to_vec();
+        let mut post_mirror: Vec<Vec<char>> = map[idx..].to_vec();
+        
+        pre_mirror.reverse();
+        if pre_mirror.len() <= post_mirror.len() {
+            post_mirror = post_mirror[0..pre_mirror.len()].to_vec();
+        } else {
+            pre_mirror = pre_mirror[0..post_mirror.len()].to_vec();
+        }
+        if pre_mirror == post_mirror {
+            return Some(idx);
+        }
+    }
+    None
+}
+
+fn part_a(input: &str) -> usize {
+    let mut answer = 0;
+    let maps: Vec<&str> = input.trim().split_terminator("\r\n\r\n").collect();
+    
+    for map_str in maps {
+        let map: Vec<Vec<char>> = map_str
+            .lines()
+            .map(|line| line.chars().collect())
+            .collect();
+        
+        match find_mirroring_line(&map) {
+            Some(x) => answer+=x*100,
+            None => {
+                match find_mirroring_line(&transpose(&map)) {
+                    Some(x) => answer+=x,
+                    None => panic!(),
+                }
+            }
+            
+        }
+    }
+
+    return answer;
+}
+
+
+fn main() {
+    let input = include_str!("input.txt");
+    let ans_a = part_a(input);
+    println!("Part A: {:?}", ans_a);
+    //29213
+}

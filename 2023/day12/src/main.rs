d519fe6417bb570a044dd18203b84b58ad182a87
Jakob <jakob.laurell@gmail.com>
2023-12-16 14:39:36 +0100

    Cleanup da12

--- 2023/day12/src/main.rs
+++ 2023/day12/src/main.rs
@@ -1,11 +1,14 @@
 use core::panic;
-use std::collections::{VecDeque, HashMap};
+use std::{collections::{VecDeque, HashMap}, time::Instant};
+use rayon::prelude::*;
+
 
 fn solve(map: & VecDeque<char>, groups: &mut VecDeque<usize>, nr_arrangements: &mut usize){
     let group = match groups.pop_front() {
         Some(x) => {x},
         None => {
-            panic!();
+            *nr_arrangements +=1;
+            return;
         },
     };
     'place_next: for place_index in 0..=map.len() as usize {
@@ -52,110 +55,10 @@ fn solve(map: & VecDeque<char>, groups: &mut VecDeque<usize>, nr_arrangements: &
 
 }
 
-const OPERATIONAL: u8 = b'.';
-const DAMAGNED: u8 = b'#';
-const UNKOWN: u8 = b'?';
 
-fn arragments(data: &[u8], groups: &[u32])-> u64 {
-    let mut cache = HashMap::new();
-    let ans = dfs(&mut cache, data, groups, 0, 0, 0);
-    ans
-}
-
-fn dfs(
-    cache: &mut HashMap<(usize, usize, u32), u64>,
-    data: &[u8],
-    groups: &[u32],
-    from: usize,
-    group: usize,
-    size: u32,
-) -> u64 {
-    if from >= data.len() {
-        if group >= groups.len() {
-            return 1;
-        }
-
-        if group == groups.len() - 1 && groups[group]==size {
-            return 1;
-        }
-
-        return 0;
-    }
-
-    match data[from] {
-        OPERATIONAL => {
-            if size == 0 {
-                return dfs(cache, data, groups, from+1, group, size);
-            }
-
-            if group >= groups.len() || size != groups[group] {
-                return 0;
-            }
-            return dfs(cache, data, groups, from+1, group+1, 0);
-        }
-        DAMAGNED => {
-            if group >= groups.len() || size + 1 > groups[group]{
-                return 0;
-            }
-
-            return dfs(cache, data, groups, from + 1 , group, size+1);
-        }
-        UNKOWN => {
-            if let Some(answer) = cache.get(&(from, group, size)).copied() {
-                return answer;
-            }
-
-            let mut ways = 0;
-            if size == 0 {
-                ways += dfs(cache, data, groups, from +1, group, size);
-            }
-
-            if group < groups.len() && size < groups[group] {
-                ways += dfs(cache, data, groups, from +1, group, size +1);
-            }
-
-            if group < groups.len() && size == groups[group] {
-                ways += dfs(cache, data, groups, from +1, group +1, 0);
-            }
-            cache.insert((from, group, size), ways);
-            return ways;
-        }
-        _ => unreachable!()
-        
-    }
-}
-
-fn part_a(input: &str, nr_folds: usize) -> u64 {
-    input.lines()
-        .map(|l| l.rsplit_once(' ').unwrap())
-        .map(|(l, r)| {
-            (
-                l.trim(),
-                r.trim()
-                    .split(',')
-                    .map(|x| x.parse().unwrap())
-                    .collect::<Vec<_>>(),
-            )
-        })
-        .map(|(data, groups)| {
-            let mut expanded_data = String::new();
-            for _ in 0..nr_folds-1 {
-                expanded_data.push_str(data);
-                expanded_data.push_str("?");
-            }
-            expanded_data.push_str(data);
- 
-            let expanded_groups = groups.repeat(nr_folds);
-            (expanded_data, expanded_groups)
-        })
-        .map(|(data, groups)| arragments(data.as_bytes(), &groups))
-        .sum()
-}
-
-
-fn part_a_old(input: &str, nr_folds: usize) -> usize {
+fn part_a(input: &str, nr_folds: usize) -> usize {
     let mut answer = 0;
-    let lines = input
+    let lines: Vec<(String, Vec<usize>)> = input
         .lines()
         .map(|l| l.rsplit_once(' ').unwrap())
         .map(|(l, r)| {
@@ -177,27 +80,34 @@ fn part_a_old(input: &str, nr_folds: usize) -> usize {
  
             let expanded_groups = groups.repeat(nr_folds);
             (expanded_data, expanded_groups)
-        });
+        }).collect();
 
-        
-    for (idx, (map, groups)) in lines.enumerate() {
+    
+    let answer: usize = lines
+    .par_iter() // Use par_iter() instead of iter() for parallel iteration
+    .enumerate()
+    .map(|(idx, (map, groups))| {
         println!("idx {}", idx);
         let mut nr_arrangements: usize = 0;
         let mut map = map.chars().collect();
-        let mut groups:VecDeque<usize> = groups.clone().into();
+        let mut groups: VecDeque<usize> = groups.clone().into();
         solve(&map, &mut groups, &mut nr_arrangements);
-        answer += nr_arrangements;
-    }
+        nr_arrangements
+    })
+    .sum();
     return answer;
 }
 
 fn main() {
     let input = include_str!("input.txt");
-    let ans_a = part_a_old(input, 1);
-    println!("Part A: {:?}", ans_a);
+    //let ans_a = part_a(input, 1);
+    //println!("Part A: {:?}", ans_a);
 
-    let ans_b = part_a_old(input, 5);
+    let start_time = Instant::now();
+    let ans_b = part_a(input, 5);
     println!("Part B: {:?}", ans_b);
+    let elapsed_time = start_time.elapsed();
+    println!("Elapsed time: {:?}", elapsed_time);
 }
 
 
@@ -209,44 +119,20 @@ mod tests {
     fn ex0() {
         let input = "???.### 1,1,3";
         assert_eq!(part_a(input, 1), 1);
-    }
-    #[test]
-    fn ex1() {
         let input = ".??..??...?##. 1,1,3";
         assert_eq!(part_a(input, 1), 4);
-    }
-    #[test]
-    fn ex2() {
         let input = "?#?#?#?#?#?#?#? 1,3,1,6";
         assert_eq!(part_a(input, 1), 1);
-    }
-    #[test]
-    fn ex3() {
         let input = "????.#...#... 4,1,1";
         assert_eq!(part_a(input, 1), 1);
-    }
-    #[test]
-    fn ex4() {
         let input = "????.######..#####. 1,6,5";
         assert_eq!(part_a(input, 1), 4);
-    }
-    #[test]
-    fn ex5() {
         let input = "?###???????? 3,2,1";
         assert_eq!(part_a(input, 1), 10);
-    }
-    #[test]
-    fn in0() {
         let input = "#?#.??????#.??????? 3,2,1,1,7";
         assert_eq!(part_a(input, 1), 3);
-    }
-    #[test]
-    fn in1() {
         let input = "????? 2,1";
         assert_eq!(part_a(input, 1), 3);
-    }
-    #[test]
-    fn in2() {
         let input = "??#?#????#?#????. 8,7";
         assert_eq!(part_a(input, 1), 1);
         let input = "..????..?????? 1,3";
@@ -256,6 +142,15 @@ mod tests {
         let input = "?.?????#???????? 1,4,2,1";
         assert_eq!(part_a(input, 1), 61);
     }
-    
+    #[test]
+    fn example_a() {
+        let input = include_str!("example.txt");
+        assert_eq!(part_a(input, 1), 21);
+    }
+    #[test]
+    fn example_b() {
+        let input = include_str!("example.txt");
+        assert_eq!(part_a(input, 5), 525152);
+    }
 
 }

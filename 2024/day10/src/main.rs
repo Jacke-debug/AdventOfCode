b759b1f0b3d1b66db9663e7bd032e412fb15fdb6
Jakob Laurell <jakob.laurell@unjo.com>
2024-12-10 20:31:45 +0100

    Solve day10 part b, part a solved by edit

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -10,6 +10,29 @@ fn _print_map(map: &[Vec<isize>]) {
     println!();
 }
 
+fn check_pos(map: &[Vec<isize>], pos: (usize, usize), i: isize, score: &mut usize) {
+    for dir in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+        let x_new = pos.0 as isize + dir.0;
+        let y_new = pos.1 as isize + dir.1;
+        if x_new >= 0 && x_new < map.len() as isize &&
+        y_new >= 0 && y_new < map[0].len() as isize {
+            let x_new = x_new as usize;
+            let y_new = y_new as usize;
+            match map[x_new][y_new] {
+                n => {
+                    if n == 9 && i == 9 {
+                        println!("{} {} {}", x_new, y_new, i);
+                        *score += 1;
+                    } else if n == i {
+                        println!("checking {} {} {}", x_new, y_new, i);
+                        check_pos(&map, (x_new, y_new), i + 1, score);
+                    }
+                }
+            }
+        }
+    }
+}
+
 fn part_a(input: &str) -> isize {
     let mut map = Vec::new();
     for line in input.lines() {
@@ -23,31 +46,16 @@ fn part_a(input: &str) -> isize {
     _print_map(&map);
     for y in 0..map.len() {
         for x in 0..map[y].len() {
-            let mut pos = (x, y);
-            for i in 0..=9 {
-                for dir in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-                    let x_new = pos.0 as isize + dir.0;
-                    let y_new = pos.1 as isize+ dir.1;
-                    if x_new >= 0 && x_new < map.len() as isize &&
-                    y_new >= 0 && y_new < map[0].len() as isize {
-                        let x_new = x_new as usize;
-                        let y_new = y_new as usize;
-                        match map[x_new][y_new] {
-                            n => {
-                                if n != i {
-                                    continue;
-                                } else {
-                                    pos = (x_new, y_new);
-                                }
-                            }
-                        }
-                    }
-                }
+            if map[x][y] != 0 {
+                continue;
             }
-            paths += 1;
+            let pos = (x, y);
+            println!("Start from {} {}", x, y);
+            check_pos(&map, pos, 1, &mut paths);
+            println!("Score {}", paths);
         }
     }
-    paths
+    paths.try_into().unwrap()
 }
 
 fn part_b(input: &str) -> isize {
